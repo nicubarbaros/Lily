728976011d47c3ce0e09d7916efff71f68e23941
Barbaros Nicolae <barbarosnico@MacBook-Pro.local>
2017-03-28 13:46:57 +0300

    feat(css): uglify js with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 var gulp = require('gulp')
-, uglify = require("gulp-uglify")
-, concat = require("gulp-concat");
+, uglify = require('gulp-uglify')
+, concat = require('gulp-concat')
+, cleanCSS = require('gulp-clean-css');
 
 var getVersion = function () {
     info = require("./package.json");
     return info.version;
 };
 
-gulp.task('build', function () {
+gulp.task('minify-js', function () {
     gulp.src('./lily.js')
     .pipe(uglify({preserveComments:'some'}))
     .pipe(concat('lily.min.js'))
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['build']);
+gulp.task('minify-css', function() {
+  gulp.src('./lily.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(concat('lily.min.css'))
+  .pipe(gulp.dest('./'));
+})
+
+gulp.task('default', ['minify-js', 'minify-css']);
